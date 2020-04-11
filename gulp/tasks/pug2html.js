76bc84c4cddd720fbc83ed0c91b05694e267d52e
Amir-Ajtz <amirajtz@yandex.ru>
2020-04-11 22:54:34 +0600

    initial commit

---
+++ gulp/tasks/pug2html.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp')
+const plumber = require('gulp-plumber')
+const htmlValidator = require('gulp-w3c-html-validator')
+const bemValidator = require('gulp-html-bem-validator')
+
+module.exports = function pug2html() {
+  return gulp.src('src/*.html')
+    .pipe(plumber())
+    .pipe(htmlValidator())
+    .pipe(bemValidator())
+    .pipe(gulp.dest('build'))
+}
+
+ 
